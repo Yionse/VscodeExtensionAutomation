eb70c3638ff138a3d43ad42472d00a7ddee0e9fc
v-weiyding <v-weiyding@microsoft.com>
2025-02-17 16:20:04 +0800

    log

--- src/common/showCurrentDirectoryList.js
+++ src/common/showCurrentDirectoryList.js
@@ -3,6 +3,7 @@ const { getScriptRoot } = require("./getScriptRoot")
 const vscode = require("vscode")
 const { execSync } = require("child_process")
 const { message } = require("./message")
+const { log } = require("./log")
 
 function showCurrentDirectoryList() {
   const scriptPath = getScriptRoot("getRootDirectoryFileList.js")
@@ -16,13 +17,16 @@ function showCurrentDirectoryList() {
       arr.forEach((item) => {
         if (item.type === "file") {
           message.file(item.file, true)
+          log("file: " + item.file, "info")
         } else {
           message.directory(item.file, true)
+          log("dict: "+item.file, "info")
         }
       })
     }
   } catch (error) {
     message.error(`Execution error: ${error.message}`)
+    log(`Execution error: ${error.message}`, "error")
   }
 }
 
--- src/createTypespecProject/pre.js
+++ src/createTypespecProject/pre.js
@@ -1,12 +1,15 @@
 const { message } = require("../common/message")
 const { execSync } = require("child_process")
+const { log } = require("../common/log")
 
 const preCheck = () => {
   message.start(`Checking the environment`)
+  log(`Checking the environment`)
   // node
   const nodeVersion = execSync("node -v").toString().trim()
   if (parseInt(nodeVersion.split(".")?.[0].replace("v", "")) < 20) {
     message.error("Node version is too low")
+    log(`Node version is too low`, "error")
     return false
   }
   execSync("npm install -g @typespec/compiler")
